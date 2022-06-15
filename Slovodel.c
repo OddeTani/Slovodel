375b781b44da2155b7232ab4f7c297ad6942c266
OddeTani <103641285+OddeTani@users.noreply.github.com>
2022-06-16 00:55:17 +0300

    PR_SP

---
+++ Slovodel.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int SlovoDel()
+{
+	char Rul[2][26] =
+	{
+	{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
+	{1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6}
+	};
+	char str[30];
+	printf("Игрок 1 вводит слово: ");
+	scanf_s("%s", str, 30);
+	int L;
+	int Res1 = 0;
+	int Res2 = 0;
+	L = strlen(str);
+	for (int i = 0; i < L; i++)
+	{
+		for (int j = 0; j < 26; j++)
+		{
+			if (str[i] == Rul[0][j]) Res1 += Rul[1][j];
+		}
+	}
+	printf("Игрок 2 вводит слово: ");
+	scanf_s("%s", str, 30);
+	L = strlen(str);
+	for (int i = 0; i < L; i++)
+	{
+		for (int j = 0; j < 26; j++)
+		{
+			if (str[i] == Rul[0][j]) Res2 += Rul[1][j];
+		}
+	}
+	printf("Результат игрока 1 = %d\n", Res1);
+	printf("Результат игрока 2 = %d\n", Res2);
+	if (Res1 > Res2) printf("Игрок 1 победил!!!");
+	else if (Res2 > Res1) printf("Игрок 2 победил!!!");
+	else printf("Победила дружба (все проиграли)");
+}
+
+int main(void)
+{
+	SlovoDel();
+}
